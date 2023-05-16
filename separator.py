cb243f827d805e88f72ef18aa9f573a4a7e19cba
ViktoryaK <96166901+ViktoryaK@users.noreply.github.com>
2023-05-16 22:11:52 +0300

    Update separator.py

--- separator.py
+++ separator.py
@@ -9,18 +9,32 @@ import soundfile as sf
 import os
 # from IPython.display import Audio
 
-def decompose(spectogram: np.ndarray, n_instruments: int, variant=''):
+
+
+
+def decompose(V: np.ndarray, n_instruments: int, variant='', number_of_iterations=20):
     n_components = n_instruments * 4
 
     if variant == 'librosa':
-        W, H = librosa.decompose.decompose(magnitude, n_components=n_components, sort=True)
+        W, H = librosa.decompose.decompose(V, n_components=n_components, sort=True)
     else:
-        pass
-
+        W = np.random.uniform(0, 1, (V.shape[0], n_components))
+        H = np.random.uniform(0, 1, (n_components, V.shape[1]))
+        for iteration in range(number_of_iterations):
+            WTV = W.T@V
+            WTWH = W.T@W@H
+            for i in range(H.shape[0]):
+                for j in range(H.shape[1]):
+                    H[i][j] = H[i][j] * WTV[i][j] / WTWH[i][j]
+            VHT = V@H.T
+            WHHT = W@H@H.T
+            for i in range(W.shape[0]):
+                for j in range(W.shape[1]):
+                    W[i][j] = W[i][j] * VHT[i][j] / WHHT[i][j]
     return W, H
 
 if __name__ == '__main__':
-    name_input = 'test_Pathway020.mp3'
+    name_input = 'test_Pathway.wav'
     file_name, file_extension = name_input.split('.')
     path = './to_separate/' + name_input
 
@@ -33,11 +47,11 @@ if __name__ == '__main__':
     spectrogram = librosa.stft(audio_sample, n_fft=n_fft, hop_length=hop_length)
     magnitude, phase = librosa.magphase(spectrogram)
 
-    W, H = decompose(magnitude, n_instruments, variant='librosa')
+    W, H = decompose(magnitude, n_instruments, variant='our')
 
     V = np.dot(W, H)
     V = np.multiply(V, phase)
     recovered_signal = librosa.istft(V, n_fft=n_fft, hop_length=hop_length)
 
-    os.mkdir(f'./separated/{file_name}/')
+    os.mkdir(f'./separated/{file_name}')
     sf.write(f'./separated/{file_name}/recovered.wav', recovered_signal, sampling_rate)
