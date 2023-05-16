8e289f5e4eefe7fe36bf484b77ec1b78f31eba76
Kolotose <amazourick@gmail.com>
2023-05-17 00:03:31 +0300

    Small fixes
    Fix in decomposition function
    Fixed error, when the directory exists

--- separator.py
+++ separator.py
@@ -6,18 +6,22 @@ from scipy import signal
 import scipy
 from sklearn.decomposition import NMF
 import soundfile as sf
-import os
+import os, shutil
+import time
 # from IPython.display import Audio
 
 
 
 
-def decompose(V: np.ndarray, n_instruments: int, variant='', number_of_iterations=20):
+def decompose(V: np.ndarray, n_instruments: int, variant='', number_of_iterations=100):
     n_components = n_instruments * 4
     W = np.random.uniform(0, 1, (V.shape[0], n_components))
     H = np.random.uniform(0, 1, (n_components, V.shape[1]))
+    # cycle_t = time.time()
+
     if variant == 'librosa':
         W, H = librosa.decompose.decompose(V, n_components=n_components, sort=True)
+
     elif variant == 'euclidian':
         for iteration in range(number_of_iterations):
             WTV = W.T@V
@@ -30,30 +34,40 @@ def decompose(V: np.ndarray, n_instruments: int, variant='', number_of_iteration
             for i in range(W.shape[0]):
                 for j in range(W.shape[1]):
                     W[i][j] = W[i][j] * VHT[i][j] / WHHT[i][j]
+            print(time.time() - cycle_t)
+            # cycle_t = time.time()
+
     elif variant == 'divergence':
         V = np.float64(V)
-        print(V.shape)
-        print(H.shape)
-        print(W.shape)
+        # print(V.shape)
+        # print(H.shape)
+        # print(W.shape)
+
         for iteration in range(number_of_iterations):
+            W_new, H_new = W.copy(), H.copy()
+            WH = W@H
             for i in range(H.shape[0]):
                 for j in range(H.shape[1]):
                     numerator = 0
                     for row in range(V.shape[0]):
-                        numerator += W[row][i]@V[row][j] / (W@H)[row][j]
+                        numerator += W[row][i]*V[row][j] / (WH)[row][j]
                     denominator = 0
                     for row in range(W.shape[0]):
                         denominator += W[row][i]
-                    H[i][j] = H[i][j] * numerator / denominator
+                    H_new[i][j] = H[i][j] * numerator / denominator
             for i in range(W.shape[0]):
                 for j in range(W.shape[1]):
                     numerator = 0
                     for column in range(V.shape[1]):
-                        numerator += H[j][column]@V[i][column] / (W@H)[i][column]
+                        numerator += H[j][column]*V[i][column] / (WH)[i][column]
                     denominator = 0
                     for column in range(H.shape[1]):
                         denominator += H[j][column]
-                    W[i][j] = W[i][j] * numerator / denominator
+                    W_new[i][j] = W[i][j] * numerator / denominator
+            W, H = W_new, H_new
+            # print(time.time() - cycle_t)
+            # cycle_t = time.time()
+
     return W, H
 
 if __name__ == '__main__':
@@ -70,11 +84,16 @@ if __name__ == '__main__':
     spectrogram = librosa.stft(audio_sample, n_fft=n_fft, hop_length=hop_length)
     magnitude, phase = librosa.magphase(spectrogram)
 
-    W, H = decompose(magnitude, n_instruments, variant='divergence')
+    W, H = decompose(magnitude, n_instruments, variant='euclidian')
 
     V = np.dot(W, H)
     V = np.multiply(V, phase)
     recovered_signal = librosa.istft(V, n_fft=n_fft, hop_length=hop_length)
 
-    os.mkdir(f'./separated/{file_name}')
+    try:
+        os.mkdir(f'./separated/{file_name}')
+    except FileExistsError:
+        shutil.rmtree(f'./separated/{file_name}')
+        os.mkdir(f'./separated/{file_name}')
+
     sf.write(f'./separated/{file_name}/recovered.wav', recovered_signal, sampling_rate)
